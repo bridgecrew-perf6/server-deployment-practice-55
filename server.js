5f1e9fd918f49ccc37c2d4af794ebf46d9d141e2
Simon Panek <paneks19@gmail.com>
2020-12-07 18:53:41 -0800

    uncommented const PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 require('dotenv').config();
 const express = require('express'); // server
 const app = express();
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // local files
 const notFoundHandler = require('./handlers/404');
@@ -39,8 +39,8 @@ function renderData(req, res, next){
 
 
 // turning server on
-function start(port) {
-  app.listen(port, () => console.log(`server is listening on ${port}`));
+function start(PORT) {
+  app.listen(PORT, () => console.log(`server is listening on ${port}`));
 }
 
 module.exports = {
